10eb1be4a2fa7552919f5c82c78a46d21079c6b8
anniejiwon <anniejiwon@gmail.com>
2016-11-16 18:11:04 -0500

    added react redux

---
+++ browser/app.jsx
@@ -0,0 +1,13 @@
+'use strict'
+import React from 'react'
+import {render} from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './store'
+
+
+render (
+  <Provider store={store}>
+  </Provider>,
+  document.getElementById("main")
+)
