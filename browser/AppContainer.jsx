dc36fae5034fd1e28dce0ea8aff57bfdc4283976
freda nada <fredasnada@gmail.com>
2016-11-17 14:01:11 -0500

    created audio source component

--- browser/AppContainer.jsx
+++ browser/AppContainer.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react'
 import { AudioContextComponent } from 'react-audio'
 
+var Context = window.AudioContext || window.webkitAudioContext
+var context = new Context();
+
+function loadDogSound(url) {
+  var request = new XMLHttpRequest();
+  request.open('GET', '/sound-file.wav', true);
+  request.responseType = 'arraybuffer';
+
+  // Decode asynchronously
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      dogBarkingBuffer = buffer;
+    }, onError);
+  }
+  request.send();
+}
+
+// function playSound(buffer) {
+//   var source = context.createBufferSource(); 
+//   source.buffer = buffer;                 
+//   source.connect(context.destination);
+//   source.start(0);                          
+// }
+
 export default class AppContainer extends Component {
+	componentDidMount () {
+		playSound()
+	}
 	render(){
-		<AudioContextComponent />
-	
+		<AudioContextComponent audioContext={context} >
+			<Delay />
+		</AudioContextComponent>	
 	}
 }
+
+
 
