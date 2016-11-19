c139ab7a69da202c2523cccca6c93f8c4de5451e
freda nada <fredasnada@gmail.com>
2016-11-19 00:47:07 -0500

    added sounds

--- public/js/sound.js
+++ public/js/sound.js
@@ -1,88 +1,54 @@
-// import store from './store'
 
-// what does below do
-//store.subscribe()
-// need state at time play button is hit
-//store.getState()
-
-// set an audiocontext
-// window.AudioContext = window.AudioContext || window.webkitAudioContext;
-// var context = new AudioContext();
-// var testSample = null;
-
-// function load(url) {
-//   var request = new XMLHttpRequest();
-//   request.open('GET', url, true);
-//   request.responseType = 'arraybuffer';
-
-//   // Decode asynchronously
-//   request.onload = function() {
-//     context.decodeAudioData(request.response, function(buffer) {
-//       testSample = buffer;
-//     }, onError);
-//   }
-//   request.send();
-// }
-
-// function playSound(buffer) {
-//   var source = context.createBufferSource();
-//   source.buffer = buffer;             
-//   source.connect(context.destination);     
-//   source.start(0);                       
-// }
-
+// loads up all the buffers
 Tone.Buffer.on('load', function(){
-// load sound files
-	// load('/sounds/heaven_vox.wav');
-// all buffers are loaded. 
-// set sources to buffer.
-
 })
 
-// what to schedule? need to know from store state 
-// when to schedule? when placing piece (schedule that piece) but also play piece one time (use scheduleOnce)
-	// when moving the piece around, use eventId to change the time it is played
-// for each loaded sound, need to schedule below what do to and at what time
-// store state [{what it is, what time}]
-// let eventId = Tone.Transport.schedule(function(time){
-//     //invoked when the Transport starts
-
-//     sourceThing = {what it is}
-//     source.start(time)
-
-// }, {what time});
-// set second param 'time' based on where object is in grid (in seconds)
-// .schedule returns an unique id that can be put on store state
-
-//delete tone if removed
-//Tone.Transport.clear(event);
-
-// onClick event listens for play
-// if store says playing run below
-//Tone.Transport.start();
-
-// when do we need to call stop? a) when stop clicked b) when end is reached
-//Tone.Transport.stop()
-
-
-
-var sampler = new Tone.Sampler("./sounds/heaven_vox.wav", function(){
-	//repitch the sample down a half step
-	sampler.triggerAttack(-5);
-}).toMaster();
-
-var player = new Tone.Player("./sounds/heaven_vox.wav").toMaster();
+// sample players
+var sample1 = new Tone.Player("./sounds/aura_arp_pad.wav").toMaster();
 //play as soon as the buffer is loaded
 // player.autostart = true;
+var sample2 = new Tone.Player("./sounds/128_beat_1.wav").toMaster();
+var sample3 = new Tone.Player("./sounds/bring_me_pad.wav").toMaster();
+var sample4 = new Tone.Player("./sounds/bringing_me_dolphins_fx.wav").toMaster();
+var sample5 = new Tone.Player("./sounds/heaven_vox.wav").toMaster();
 
-Tone.Transport.schedule(function(time){
-	player.start();
+
+// effects
+var distortion = new Tone.Distortion(0.3).toMaster();
+var pitchShift = new Tone.PitchShift (-2).toMaster();
+var reverb = new Tone.JCReverb(0.4).toMaster();
+var vibra = new Tone.Vibrato(10, 0.5).toMaster()
+// effect connections
+
+
+// scheduling
+Tone.Transport.scheduleOnce(function(time){
+	sample1.connect(pitchShift);
+	sample1.start();
 }, 0);
+Tone.Transport.scheduleRepeat(function(time){
+	sample2.start();
+}, '1m', 2, 12);
+Tone.Transport.scheduleRepeat(function(time){
+	sample3.start();
+}, '1m', 1, 10);
+Tone.Transport.schedule(function(time){
+	sample4.start();
+}, 5);
+Tone.Transport.schedule(function(time){
+	sample4.connect(reverb);
+}, 7);
+Tone.Transport.schedule(function(time){
+	sample5.connect(vibra).start();
+}, 4);
 
 document.querySelector("#theButton").addEventListener("click", function(){
-  //get the current time
-  // var now = Tone.now();
   Tone.Transport.start();
-  //schedule relative to 'now'
-	// playSound(testSample)
 });
+
+// basic scheduling function
+function schedule (sample, playTime) {
+	return Tone.Transport.schedule(function(time){
+		sample.start();
+	}, playTime);
+}
