c74e538c506a522a74e604e52e592b964551830c
freda nada <fredasnada@gmail.com>
2016-11-21 16:21:27 -0500

    completed slide out nav

--- js/interface.js
+++ js/interface.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("chevron-right").style.display ='none';
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("chevron-right").style.display ='block';
 }
