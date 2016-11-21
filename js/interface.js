a3dc6acc170da20a8424264178a7b698b75f84b3
freda nada <fredasnada@gmail.com>
2016-11-21 16:40:36 -0500

    rm interface.js

--- js/interface.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("chevron-right").style.display ='none';
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("chevron-right").style.display ='block';
-}
