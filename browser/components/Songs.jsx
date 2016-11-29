39ca6cc75f6b9adc83e663ff704b01947002b234
anniejiwon <anniejiwon@gmail.com>
2016-11-29 11:06:19 -0500

    songs

---
+++ browser/components/Songs.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+export default ({}) => (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy" style={{backgroundImage:`http://www.clipartkid.com/images/472/neon-musical-notes-background-clipart-panda-free-clipart-images-t8rkdw-clipart.png`}}></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+            <div className="col-md-3 col-sm-4 col-xs-6">
+                <div className="dummy"></div>
+                <a href="#" className="thumbnail purple">Songs from backend</a>
+            </div>
+        </div>
+    </div>
+);
+
+
