b3a059333e383d43f12882164a75e0a9dc4d08be
anniejiwon <anniejiwon@gmail.com>
2016-11-30 12:02:51 -0500

    twitter button works

--- browser/components/Controls.jsx
+++ browser/components/Controls.jsx
@@ -100,7 +100,14 @@ export class Controls extends Component {
 		this.setState({samples:[], eventIds:[]});
 	}
 
+	_handleTwitter() {
+		window.open("https://twitter.com/share")
+	}
+	
+
+
 	render () {
+		const {_handleTwitter} = this
 		return (
 		<div>
 			<div id='controls'>
@@ -147,12 +154,10 @@ export class Controls extends Component {
 				</svg>
 
 				{/* upload button */}
-				<svg fill="rgba(86, 101, 115, 0.7)" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-				<path d="M0 0h24v24H0z" fill="none"/>
+				<svg fill="rgba(86, 101, 115, 0.7)" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" onClick={_handleTwitter}>
+					<path d="M0 0h24v24H0z" fill="none"/>
 					<path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
 				</svg>
-				
-
 		  </div>
   	</div>
 	  
