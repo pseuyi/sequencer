63f2554dcd7b6c8e2a39b19af29037b8c77151be
Nisreen Al-Basha <albasha.nisreen@gmail.com>
2016-12-01 17:51:34 -0500

    added a PORT and nodemon

--- server/start.js
+++ server/start.js
@@ -28,16 +28,24 @@ module.exports = app
   // Send index.html for anything else.
   .get('/*', (_, res) => res.sendFile(resolve(__dirname, '..', 'public', 'index.html')))
 
-if (module === require.main) {
+// if (module === require.main) {
   // Start listening only if we're the main module.
   // 
   // https://nodejs.org/api/modules.html#modules_accessing_the_main_module
-  const server = app.listen(
-    process.env.PORT || 1337,
+  
+  const PORT = process.env.PORT || 1337;
+    // app.listen(PORT, () => {
+    //     console.log(`Our app is running on port ${ PORT }`);
+    // });
+  app.listen(
+    PORT,
     () => {
     //   console.log(`--- Started HTTP Server for ${pkg.name} ---`)      
-      console.log(`Listening on ${JSON.stringify(server.address())}`)
+      console.log(`Listening on ${PORT}`)
     }
   )
-}
+// }
 
+// app.listen(process.env.PORT || 3000, function(){
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
