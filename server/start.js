baad86a9933a9aaaa5b1302542be9cceb56fae41
Nisreen Al-Basha <albasha.nisreen@gmail.com>
2016-12-01 18:28:09 -0500

    added engines to package.json

--- server/start.js
+++ server/start.js
@@ -49,3 +49,18 @@ module.exports = app
 // app.listen(process.env.PORT || 3000, function(){
 //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 // });
+
+// var express = require('express');
+// var app = express();
+
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+// app.get(/^(.+)$/, function (req, res) {
+//     res.sendFile(__dirname + req.params[0]);
+// });
+
+// var PORT = process.env.PORT || 3000;
+
+// app.listen(PORT);
