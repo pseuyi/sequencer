7430bbfda02ebfd510746ff69648260d407f3761
anniejiwon <anniejiwon@gmail.com>
2016-12-03 18:38:14 -0500

    fixed splash page for actual size display

--- browser/components/Splash.jsx
+++ browser/components/Splash.jsx
@@ -35,7 +35,7 @@ export default class Splash extends Component {
 							ENTER
 							</p>
 						</div>
-						<p id="description3" >Click <svg fill="rgba(86, 101, 115, 0.7)" height="40" viewBox="0 0 24 24" width="40" xmlns="http://www.w3.org/2000/svg" onClick={this.props.toggleSplashPage}>
+						<p id="description3" >Click <svg fill="rgba(86, 101, 115, 0.7)" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg" onClick={this.props.toggleSplashPage}>
 					<path d="M0 0h24v24H0z" fill="none"/>
 					<path d="M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z"/>
 				</svg> for instructions</p>
--- public/bundle.js
+++ public/bundle.js
@@ -34376,7 +34376,7 @@
 								'Click ',
 								_react2.default.createElement(
 									'svg',
-									{ fill: 'rgba(86, 101, 115, 0.7)', height: '40', viewBox: '0 0 24 24', width: '40', xmlns: 'http://www.w3.org/2000/svg', onClick: this.props.toggleSplashPage },
+									{ fill: 'rgba(86, 101, 115, 0.7)', height: '20', viewBox: '0 0 24 24', width: '20', xmlns: 'http://www.w3.org/2000/svg', onClick: this.props.toggleSplashPage },
 									_react2.default.createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' }),
 									_react2.default.createElement('path', { d: 'M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' })
 								),
