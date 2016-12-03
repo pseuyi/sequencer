a87d5f0730ede4012e2621296b39714c6a4b9e23
freda nada <fredasnada@gmail.com>
2016-12-03 17:36:11 -0500

    fix reload bug

--- browser/components/Patterns.jsx
+++ browser/components/Patterns.jsx
@@ -5,17 +5,25 @@ import * as firebase from 'firebase';
 import { deleteSong, togglePatternPage, loadPattern } from '../reducers/timelineReducer';
 
 export class Patterns extends React.Component {
-    constructor () {
-        super()
+    constructor (props) {
+        super(props)
+        this.state = {
+            loading: true
+        }
         this.loading = this.loading.bind(this);
     }
 
    componentWillMount() {
         this.props.fetchSongs();
+    }
+   componentDidMount () {
+        if(this.state.loading) {
         // runs once to check that firebase has data, then removes the load text
-        firebase.database().ref('/songs/').once('value', () => {
-            document.getElementById('loadText').remove();
-        })
+            firebase.database().ref('/songs/').once('value', () => {
+                document.getElementById('loadText').remove();
+                this.setState({loading: false})
+            })
+        }
     }
 
     loading (song) {
@@ -29,13 +37,15 @@ export class Patterns extends React.Component {
     // }
 
     render() {
-        console.log("SONGS----", Array.isArray(this.props.songs))
+        console.log('loading correctly?', this.state.loading)
         return (
             <div id='pattern-modal' className="container">
             
                 <div className="row">
                 <div id='close-btn-container'>
-                <div id='loadText' className="loading">loading patterns...</div>
+                { this.state.loading?
+                    <div id='loadText' className="loading">loading patterns...</div> : null
+                }
                     <button id='close-btn' onClick={this.props.togglePatternPage}>close</button>
                  </div>
             </div>
