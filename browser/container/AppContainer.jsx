86654bb55a90659295be61471790f0351cdab663
freda nada <fredasnada@gmail.com>
2016-12-06 11:29:46 -0500

    update h1 size

--- browser/container/AppContainer.jsx
+++ browser/container/AppContainer.jsx
@@ -72,7 +72,7 @@ export class AppContainer extends React.Component {
         { this.props.patternPage && !this.props.savePage? <PatternsContainer /> : <div></div> }
         { this.props.savePage && !this.props.patternPage? <Save /> : <div></div> }
         {/* { this.props.splashPage ? <Splash /> : <div></div> } */}
-        { this.props.instructionsPage ? <Instructions /> : <div></div> }
+        { this.props.instructionsPage? <Instructions /> : <div></div> }
         { this.props.isPlaying? <Progress />:  <Navigation /> } 
         <div>
           <Renderer
--- public/bundle.js
+++ public/bundle.js
@@ -52908,7 +52908,7 @@
 						),
 						_react2.default.createElement(
 							'svg',
-							{ fill: 'rgba(86, 101, 115, 0.7)', height: '24', viewBox: '0 0 24 24', width: '24', xmlns: 'http://www.w3.org/2000/svg', onClick: this.props.savePage ? null : this.props.toggleInstructionsPage },
+							{ fill: 'rgba(86, 101, 115, 0.7)', height: '24', viewBox: '0 0 24 24', width: '24', xmlns: 'http://www.w3.org/2000/svg', onClick: this.props.savePage || this.props.patternPage ? null : this.props.toggleInstructionsPage },
 							_react2.default.createElement('path', { d: 'M0 0h24v24H0z', fill: 'none' }),
 							_react2.default.createElement('path', { d: 'M11 17h2v-6h-2v6zm1-15C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zM11 9h2V7h-2v2z' })
 						),
